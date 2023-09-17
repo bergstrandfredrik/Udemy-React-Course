cf6d1d7797a5e44ee836ba84d236a0e5c771e581
Fredrik Bergstrand <fred.bergstrand@gmail.com>
2023-09-17 16:17:52 +0200

    Fix css in modal component

--- practice-project2/src/UI/modal.js
+++ practice-project2/src/UI/modal.js
@@ -4,8 +4,11 @@ const Modal = () => {
   return (
     <div className={styles.modal}>
       <div className={styles["modal-box"]}>
-        <p>Invalid input</p>
-        <button>Back</button>
+        <div className={styles["modal-header"]}>Invalid input</div>
+        <div className={styles["modal-div"]}>Please enter a valid age (>0)</div>
+        <div className={styles["modal-button"]}>
+          <button>Okay</button>
+        </div>
       </div>
     </div>
   );
